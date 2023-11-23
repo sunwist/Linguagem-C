3725e99532e5c69dd90c7f55966987a8b56ab1af
jenni <oliveirajennifer2648@gmail.com>
2023-11-23 19:40:19 -0300

    Update structs-vetor.c

--- structs-vetor.c
+++ structs-vetor.c
@@ -12,15 +12,14 @@
 
 // 02. Crie uma função que peça ao usuário um número de 1 até 5.
 // Em seguida, seu programa deve exibir informações da banda cuja posição no seu ranking
-//é a que foi solicitada pelo usuário.
+// é a que foi solicitada pelo usuário.
 
 // 03. Crie uma função em C que peça ao usuário um tipo de música e exiba as bandas com esse
-//tipo de música no seu ranking. Que função da string.h você usaria para comparar as strings
+// tipo de música no seu ranking. Que função da string.h você usaria para comparar as strings
 // que representam o tipo de banda?
 
 // 04. Crie uma função que peça o nome de uma banda ao usuário e diga se ela está entre suas
 // bandas favoritas ou não.
-
 
 typedef struct
 {
@@ -40,12 +39,16 @@ void cadastro()
     printf("\nBanda %d", i + 1);
     printf("\nNome: ");
     scanf(" %s", &b[i].nome);
+    fflush(stdin);
     printf("Estilo Musical: ");
     scanf(" %s", &b[i].estiloMusical);
+    fflush(stdin);
     printf("Integrantes: ");
     scanf(" %d", &b[i].integrantes);
+    fflush(stdin);
     printf("Ranking: ");
     scanf(" %d", &b[i].ranking);
+    fflush(stdin);
   }
 
   printf("\n\n------ Informações das Bandas");
@@ -61,6 +64,7 @@ void consulta()
   int num;
   printf("\nDigite um numero de um 1 a 5: ");
   scanf(" %d", &num);
+  fflush(stdin);
 
   for (i = 0; i < 5; i++)
   {
@@ -76,6 +80,7 @@ void tipoMusica()
   char escolha_musica[20];
   printf("\nDigite um estilo musical: ");
   scanf(" %s", escolha_musica);
+  fflush(stdin);
 
   for (i = 0; i < 5; i++)
   {
@@ -93,6 +98,7 @@ void favoritas()
   char nome_banda[20];
   printf("\nDigite o nome da banda: ");
   scanf(" %s", nome_banda);
+  fflush(stdin);
   for (i = 0; i < 5; i++)
   {
     if (strcmp(nome_banda, b[i].nome) == 0)
@@ -143,6 +149,7 @@ int main()
 
     printf("\n\nDeseja continuar no sistema...... [S] ou [N] ");
     scanf(" %c", &resp);
+    fflush(stdin);
     resp = toupper(resp);
   }
   return 0;
